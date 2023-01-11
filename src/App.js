384b0f2d2129818762813dafd3f625e11d8ebbf1
pxn-nauxray <nauxray@phantom.sh>
2023-01-12 02:34:00 +0800

    feat(initial): setup react and tailwindcss

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-lilac text-3xl">testing!!!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lilac: "#BBAEC6",
+        yellowLight: "#F1E9B2",
+        red: "#924949",
+      },
+    },
+  },
+  plugins: [],
+};
