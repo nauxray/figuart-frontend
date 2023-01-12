c981daebf9d9a5ea0a1a44dd1abda5ef9d296a92
pxn-nauxray <nauxray@phantom.sh>
2023-01-13 05:30:26 +0800

    fix: loader css

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,8 +2,8 @@ const Loader = ({ className: classnames = "" }) => {
   return (
     <div
       className={
-        ("w-14 h-14 rounded-[50%] border-4 border-t-transparent border-lilac animate-spin bg-black",
-        classnames)
+        "w-14 h-14 rounded-[50%] border-4 border-t-transparent border-lilac animate-spin bg-black " +
+        classnames
       }
     />
   );
