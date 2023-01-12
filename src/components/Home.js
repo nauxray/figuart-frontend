a1dc80f66fa06617f6b99ca685ed2d407082b92e
pxn-nauxray <nauxray@phantom.sh>
2023-01-13 06:54:27 +0800

    feat: add sortOptions under utils, add SearchModal component & add search function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
+
+import useModal from "../hooks/useModal";
 import Api from "../utils/api";
 import Loader from "./Loader";
+import SearchModal from "./Modal/SearchModal";
 import ProductCard from "./ProductCard";
 import SellerCard from "./SellerCard";
 
 const Home = () => {
-  const [search, setSearch] = useState("");
-  const [filters, setFilters] = useState({});
+  const { isShowing, toggle } = useModal();
+  const api = new Api();
 
   const [topProducts, setTopProducts] = useState([]);
   const [featuredSellers, setFeaturedSellers] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const [searchName, setSearchName] = useState("");
+  const [filters, setFilters] = useState({});
+  const [searchResults, setSearchResults] = useState(null);
+  const [searchLoading, setSearchLoading] = useState(false);
+
   const fetchData = async () => {
     setLoading(true);
-    const api = new Api();
     const top = await api.getTopProducts();
     setTopProducts(top);
     const featured = await api.getFeaturedSellers();
@@ -27,6 +34,26 @@ const Home = () => {
     fetchData();
   }, []);
 
+  const searchProducts = async () => {
+    setSearchLoading(true);
+    const results = await api.getProducts(
+      searchName.trim().length > 0 ? { name: searchName, ...filters } : filters
+    );
+    setSearchResults(results);
+    setSearchLoading(false);
+  };
+
+  const countFiltersApplied = () => {
+    let count = searchName.trim().length > 0 ? 1 : 0;
+    Object.keys(filters).forEach((key) => {
+      if (key === "limit" && filters[key] === 0) return;
+      if (key === "minPrice" && filters[key] === 0) return;
+      if (key === "sortBy" && filters[key] === "") return;
+      count += 1;
+    });
+    return count;
+  };
+
   return (
     <div className="w-11/12 mx-auto">
       {/* search bar */}
@@ -35,12 +62,15 @@ const Home = () => {
         <div className="bg-lilac rounded-lg overflow-hidden flex items-center pr-2">
           <input
             className="text-black box-border flex-1 outline-none mx-2 ml-4 py-2 bg-transparent"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
           />
           <CiSearch size={30} color={"black"} />
         </div>
-        <div className="flex mt-2 cursor-pointer w-fit ml-auto">
+        <div
+          className="flex mt-2 cursor-pointer w-fit ml-auto"
+          onClick={toggle}
+        >
           <img
             src="assets/icons/filter-search.svg"
             alt="filter"
@@ -48,11 +78,40 @@ const Home = () => {
           />
           {Object.keys(filters)?.length > 0 && (
             <div className="font-semibold text-xs bg-lilac rounded-md h-fit w-fit px-1 text-black border-white border">
-              {Object.keys(filters)?.length}
+              {countFiltersApplied()}
             </div>
           )}
         </div>
       </div>
+      <SearchModal
+        isShowing={isShowing}
+        hide={toggle}
+        filters={filters}
+        setFilters={setFilters}
+        searchProducts={searchProducts}
+      />
+      {/* end search bar */}
+
+      {/* search results */}
+      {searchLoading ? (
+        <div className="mx-auto w-fit my-12">
+          <Loader />
+        </div>
+      ) : (
+        searchResults !== null && (
+          <div>
+            <h1 className="font-header text-2xl text-center mb-4">
+              {searchResults?.length ?? 0} Search Results
+            </h1>
+            <div className="flex flex-wrap gap-2">
+              {searchResults?.map((product) => (
+                <ProductCard key={"search" + product.id} product={product} />
+              ))}
+            </div>
+          </div>
+        )
+      )}
+      {/* end search results */}
 
       {loading ? (
         <div className="mx-auto w-fit my-12">
@@ -60,14 +119,14 @@ const Home = () => {
         </div>
       ) : (
         <>
-          <div id="top5" className="">
+          <div id="top5" className="mt-8">
             <div className="flex gap-2 justify-center mb-6 items-center">
               <img src="/assets/icons/like.svg" alt="like" className="inline" />
               <h1 className="font-header text-3xl">Top Products</h1>
             </div>
             <div className="flex flex-wrap gap-2">
               {topProducts?.map((product) => (
-                <ProductCard key={product.id} product={product} />
+                <ProductCard key={"top" + product.id} product={product} />
               ))}
             </div>
           </div>
---
+++ src/utils/sortOptions.js
@@ -0,0 +1,46 @@
+export const sortOptions = [
+  {
+    key: "",
+    name: "Default",
+  },
+  {
+    key: "name:asc",
+    name: "Alphabetical (A-Z)",
+  },
+  {
+    key: "name:desc",
+    name: "Alphabetical (Z-A)",
+  },
+  {
+    key: "sales:asc",
+    name: "No. of sales (ascending)",
+  },
+  {
+    key: "sales:desc",
+    name: "No. of sales (descending)",
+  },
+  {
+    key: "price:asc",
+    name: "Price (ascending)",
+  },
+  {
+    key: "price:desc",
+    name: "Price (descending)",
+  },
+  {
+    key: "quantity:asc",
+    name: "Quantity (ascending)",
+  },
+  {
+    key: "quantity:desc",
+    name: "Quantity (descending)",
+  },
+  {
+    key: "created_at:asc",
+    name: "Date listed (ascending)",
+  },
+  {
+    key: "created_at:desc",
+    name: "Date listed (descending)",
+  },
+];
